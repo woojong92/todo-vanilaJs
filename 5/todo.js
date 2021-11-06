f413cf8c650293aeb7680397c6d1e45cb99e30cd
woojong92 <woojong92@gmail.com>
2021-11-06 15:40:15 +0900

    update

--- 5/todo.js
+++ 5/todo.js
@@ -1,5 +1,17 @@
 const todoInputElem = document.querySelector('.todo-input');
 const todoListElem = document.querySelector('.todo-list');
+const completeAllBtnElem = document.querySelector('.complete-all-btn');
+
+const leftItemsElem = document.querySelector('.left-items')
+
+const getActiveTodos = () => {
+    return todos.filter(todo => todo.isCompleted === false);
+}
+
+const setLeftItems = () => {
+    const leftTodos = getActiveTodos()
+    leftItemsElem.innerHTML = `${leftTodos.length} items left`
+}
 
 let todos = [];
 let id = 0;
@@ -12,17 +24,61 @@ const getAllTodos = () => {
     return todos;
 }
 
+const getCompletedTodos = () => {
+    return todos.filter(todo => todo.isCompleted === true );
+}
+
+let isAllCompleted = false; // 전체 todos 체크 여부
+
+const setIsAllCompleted = (bool) => { isAllCompleted = bool};
+
+const completeAll = () => {
+    completeAllBtnElem.classList.add('checked');
+    const newTodos = getAllTodos().map(todo => ({...todo, isCompleted: true }) )
+    setTodos(newTodos)
+}
+
+const incompleteAll = () => {
+    completeAllBtnElem.classList.remove('checked');
+    const newTodos =  getAllTodos().map(todo => ({...todo, isCompleted: false }) );
+    setTodos(newTodos)
+}
+
+// 전체 todos의 check 여부 (isCompleted)
+const checkIsAllCompleted = () => {
+    if(getAllTodos().length === getCompletedTodos().length ){
+        setIsAllCompleted(true);
+        completeAllBtnElem.classList.add('checked');
+    }else {
+        setIsAllCompleted(false);
+        completeAllBtnElem.classList.remove('checked');
+    }
+}
+
+const onClickCompleteAll = () => {
+    if(!getAllTodos().length) return; // todos배열의 길이가 0이면 return;
+
+    if(isAllCompleted) incompleteAll(); // isAllCompleted가 true이면 todos를 전체 미완료 처리 
+    else completeAll(); // isAllCompleted가 false이면 todos를 전체 완료 처리 
+    setIsAllCompleted(!isAllCompleted); // isAllCompleted 토글
+    paintTodos(); // 새로운 todos를 렌더링
+    setLeftItems()
+}
+
 const appendTodos = (text) => {
     const newId = id++;
     const newTodos = getAllTodos().concat({id: newId, isCompleted: false, content: text })
     // const newTodos = [...getAllTodos(), {id: newId, isCompleted: false, content: text }]
     setTodos(newTodos)
+    setLeftItems()
+    checkIsAllCompleted();
     paintTodos();
 }
 
 const deleteTodo = (todoId) => {
     const newTodos = getAllTodos().filter(todo => todo.id !== todoId );
     setTodos(newTodos);
+    setLeftItems()
     paintTodos()
 }
 
@@ -30,6 +86,8 @@ const completeTodo = (todoId) => {
     const newTodos = getAllTodos().map(todo => todo.id === todoId ? {...todo,  isCompleted: !todo.isCompleted} : todo )
     setTodos(newTodos);
     paintTodos();
+    setLeftItems()
+    checkIsAllCompleted();
 }
 
 const updateTodo = (text, todoId) => {
@@ -107,6 +165,10 @@ const init = () => {
             appendTodos(e.target.value); todoInputElem.value ='';
         }
     })
+
+    completeAllBtnElem.addEventListener('click',  onClickCompleteAll)
+
+    setLeftItems()
 }
 
 init()
